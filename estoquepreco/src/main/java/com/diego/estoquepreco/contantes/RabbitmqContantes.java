43bb3bba43adf5c201a11f5a0a90b4d3ac570c92
Diego Rangel <diegorangeljpa@gmail.com>
2022-04-11 15:23:26 -0300

    Ajustes de envio das mensagens para fila do rabbit

--- estoquepreco/src/main/java/com/diego/estoquepreco/contantes/RabbitmqContantes.java
+++
@@ -1,8 +0,0 @@
-package com.diego.estoquepreco.contantes;
-
-public class RabbitmqContantes {
-
-    public static final String FILA_ESTOQUE = "ESTOQUE";
-    public static final String FILA_PRECO = "PRECO";
-
-}
