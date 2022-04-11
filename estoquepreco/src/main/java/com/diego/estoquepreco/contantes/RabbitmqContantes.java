1d052ba3d34fe954b0ea38e9b40663aee9ec6c58
Diego Rangel <diegorangeljpa@gmail.com>
2022-04-11 12:47:18 -0300

    Criação do serviço que cria a fila no rabbitmq

---
+++ estoquepreco/src/main/java/com/diego/estoquepreco/contantes/RabbitmqContantes.java
@@ -0,0 +1,8 @@
+package com.diego.estoquepreco.contantes;
+
+public class RabbitmqContantes {
+
+    public static final String FILA_ESTOQUE = "ESTOQUE";
+    public static final String FILA_PRECO = "PRECO";
+
+}
